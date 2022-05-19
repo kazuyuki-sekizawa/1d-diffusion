6a0aa1c2f134f94dbdd76eecd5c2874380d25e52
Kazuyuki Sekizawa <kazuyuki.sekizawa@live.jp>
2022-05-20 08:59:13 +0900

    Delete html directory

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['1d_2ddiffusion_2ef90_0',['1d-diffusion.f90',['../1d-diffusion_8f90.html',1,'']]]
-];
--- html/search/all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['test_0',['test',['../1d-diffusion_8f90.html#afd492b7a968e07cf34987f57752e6bee',1,'1d-diffusion.f90']]],
-  ['todo_20list_1',['Todo List',['../todo.html',1,'']]]
-];
